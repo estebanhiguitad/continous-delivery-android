af8c52fef0a6a46a7eedd1402a5cb8b322ced7d1
Esteban Higuita <esteban.higuita@ceiba.com.co>
2022-01-19 16:43:35 -0500

    first commit

---
+++ app/src/main/java/co/com/esteban/continuousdelivery/MainActivity.kt
@@ -0,0 +1,41 @@
+package co.com.esteban.continuousdelivery
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import co.com.esteban.continuousdelivery.ui.theme.ContinuousDeliveryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ContinuousDeliveryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(
+        text = "Hello $name!"
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ContinuousDeliveryTheme {
+        Greeting("Android")
+    }
+}
+
